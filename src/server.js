6cb6593817b565b2b77f1683cee16cbd0856d73b
Jitendra Saini <jitendrasaini@markanthony.co.in>
2026-02-19 22:23:53 +0530

    ser

--- src/server.js
+++ src/server.js
@@ -1,23 +1,42 @@
 import app from "./app.js";
+import cluster from "cluster";
+import os from "os";
 import ENV from "./config/env.js";
 import connectDB from "./config/database.js";
 import { connectRedis } from "./config/redis.js";
 
-const startServer = async () => {
+const numCPUs = os.cpus().length;
+
+const startWorker = async () => {
   try {
     await connectDB();
     await connectRedis();
 
     app.listen(ENV.PORT, () => {
       console.log(
-        `🚀 Server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`
+        `🚀 Worker ${process.pid} listening on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`
       );
     });
-
   } catch (error) {
-    console.error("❌ Failed to start server:", error.message);
+    console.error("❌ Failed to start worker:", error);
     process.exit(1);
   }
 };
 
-startServer();
+if (cluster.isPrimary) {
+  console.log(`🧠 Primary ${process.pid} running`);
+  console.log(`📊 Forking ${numCPUs} workers...`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.warn(
+      `⚠ Worker ${worker.process.pid} died (code: ${code}, signal: ${signal}). Restarting...`
+    );
+    cluster.fork();
+  });
+} else {
+  startWorker();
+}
